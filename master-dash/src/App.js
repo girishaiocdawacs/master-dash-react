d0ef2e3175fc988b326688a795ea9408a043576f
shubhamaiocd <shubhamsnehi@aiocdawacs.com>
2021-07-07 13:06:45 +0530

    Code Update

--- master-dash/src/App.js
+++ master-dash/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/argon.css?v=1.2.0';
 
 function App() {
